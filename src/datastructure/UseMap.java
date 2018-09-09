d62c8bb040a2c9feef681391e8d9da30011b4f17
[jeniferahmed] <[jeniferahmed87@gmail.com]>
2018-09-09 16:34:51 -0400

    Core-Java all answer

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -10,7 +10,8 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
-
+//declaring a map,use value & use loop,put method,call the map - how to retrieve data from map. try for each method.
+		//gfg.forEach((k,v) -> System.out.println("Key = " + k + ", Value = " + v));
 	}
 
 }
