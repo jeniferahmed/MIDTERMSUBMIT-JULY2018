a51ee4fae8e6da1054c9601ead78147a7e4f5e27
[jeniferahmed] <[jeniferahmed87@gmail.com]>
2018-09-13 00:53:40 -0400

    Palindrome done

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,24 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String word1 = "Hi"; //No
+        String word2 = "Bob"; //Yes
+        checkPalindrome(word1);
+        checkPalindrome(word2);
+        checkPalindrome("Midterm"); //No
+        checkPalindrome("Done"); //Yes
+    }
+    public static void checkPalindrome(String word) {
+        String actual = word.toLowerCase();
+        String reverse = "";
+
+        for(int i=actual.length()-1; i>= 0; i--) {
+            reverse = reverse + actual.charAt(i);
+        }
+        if(actual.equals(reverse)){
+            System.out.println("Correct, it is a Palindrome");
+        }else{
+            System.out.println("It is not a palindrome");
+        }
     }
 }
