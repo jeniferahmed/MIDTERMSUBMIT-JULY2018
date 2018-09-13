891ba8c06e2b8f4696aac6bf30f9ce91545ea2ba
[jeniferahmed] <[jeniferahmed87@gmail.com]>
2018-09-13 01:08:39 -0400

    Permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -12,5 +12,19 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        permutation("", "XYZ");
+    }
+    public static void permutation(String prefix,String sg){
+        int a = sg.length();
+
+        if(a==0) {
+            System.out.println(prefix);
+        }else{
+            for(int i=0; i <a; i++){
+                permutation(prefix + sg.charAt(i), sg.substring(0, i) + sg.substring(i+1, a));
+
+            }
+
+        }
     }
 }
